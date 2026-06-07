464947169b2747debd134cd5818f8a0d90181fe7
Nikunj <mnikunj2622@gmail.com>
2026-06-07 11:29:43 +0530

    impl: amm

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("8NVA8HLgC9e3G4VgdfEAxPGeomNZgHFQGaAXDZsgYjHV");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
