d6958b402c900cf33ff1929d794cc3b38e8604af
Nikunj <mnikunj2622@gmail.com>
2026-06-08 18:38:20 +0530

    impl: instruction

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -15,6 +15,13 @@ declare_id!("8NVA8HLgC9e3G4VgdfEAxPGeomNZgHFQGaAXDZsgYjHV");
 pub mod amm {
     use super::*;
 
+    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
     pub fn deposit_with_introspection(
         ctx: Context<DepositWithIntrospection>,
         amount: u64,
@@ -22,6 +29,14 @@ pub mod amm {
         max_y: u64,
     ) -> Result<()> {
         ctx.accounts.deposit_with_introseption(amount, max_x, max_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount, min)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y)
     }
 
     pub fn withdraw_with_introspection(
